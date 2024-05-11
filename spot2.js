b9098ed181c3a6a3853a648aaf1a7d5c974880bc
kushtyai <kushtyagi2311@gmail.com>
2024-05-11 09:58:40 +0530

    first commit

---
+++ spot2.js
@@ -0,0 +1,18 @@
+ function auth(){
+    var email=document.getElementById('email');
+    var password=document.getElementById('password');
+
+    //condition//
+    
+ if(email=='admin@gmail.com' && password==tyagi)
+ {
+    window.location.href('spot2.html');
+    alert("loggedin")
+
+ }
+ else{
+    alert("invalid information")
+    return;
+ }
+    
+ }
